725c1319a499d420c8ffd93a875a87385e004ff4
Farid Hamida <faridhamida22@gmail.com>
2020-05-26 10:10:29 -0700

    users/messages completed

---
+++ server.js
@@ -0,0 +1,16 @@
+const path = require('path')
+const http = require('http')
+const express = require('express')
+const socketio = require('socket.io')
+const formatMessage = require('./utils/messages')
+const {
+    userJoin,
+    getCurrentUser,
+    userLeave,
+    getRoomUser
+} = require('./utils/users')
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
